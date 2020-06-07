8afdd6f0d830e537f93167deabd02d9562aad8d4
Anna Harvey <anharvey@my365.bellevue.edu>
2020-06-07 16:23:26 -0400

    Create 1.4_HarveyAnna.py

---
+++ 1.4_HarveyAnna.py
@@ -0,0 +1,38 @@
+# Assignment 1.4
+# Name: Harvey, Anna
+# Date: 2020-06-07
+
+# Display the text “Hello World! I wonder why that is always the default coding text to start with”
+print('Hello World! I wonder why that is always the default coding text to start with?')
+
+# Add two numbers together
+add = 1 + 2
+print(add)
+
+# Subtract a number from another number
+subtract = 10 - 5
+print(subtract)
+
+# Multiply two numbers
+multiply = (10*5)
+print(multiply)
+
+# Divide between two numbers
+divide = (25/5)
+print(divide)
+
+# Concatenate two strings together (any words)
+plants = ('Grow ' + 'tall.')
+print(plants)
+
+# Create a list of 4 items (can be strings, numbers, both)
+list = ['cat', 'dog', 'fish', 'turtle']
+print(list)
+
+# Append an item to your list (again, can be a string, number)
+list.append('alligator')
+print(list)
+
+# Create a tuple with 4 items (can be strings, numbers, both)
+tuple = (4, 'score', 'and', 7)
+print(tuple)
